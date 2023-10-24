8548eb19d7cb00141af0ac12837d9912166401ae
Helga Ish <belfegor.nomnom@gmail.com>
2023-10-24 15:29:06 +0800

    feat: add header and menu styles

--- subscribster/src/components/Header/Header.js
+++ subscribster/src/components/Header/Header.js
@@ -1,21 +1,29 @@
 import Menu from '../Menu/Menu';
 import React from 'react';
+import './Header.css';
 
 function Header() {
 
   const isLoggedIn = true;
 
   return (
-    <header>
-      <img />
-      {isLoggedIn ? (
-        <Menu />
-      ) : (
-        <ul>
-          <li><button>Sign in</button></li>
-          <li><button>Sign up</button></li>
-        </ul>
-      )}
+    <header className='header'>
+      <div className='header__container'>
+        <img className='logo' alt='subscribster logo' />
+        <h1 className='heading'>Subscribster</h1>
+        {isLoggedIn ? (
+          <Menu />
+        ) : (
+          <nav className='header__navigation'>
+            <ul className='header__list'>
+              <li className='header__link'><button className='header__button-signin'>Sign in</button></li>
+              <li className='header__link'><button className='header__button-signup'>Sign up</button></li>
+            </ul>
+          </nav>
+
+        )}
+
+      </div>
 
     </header>
   )
--- subscribster/src/components/Menu/Menu.js
+++ subscribster/src/components/Menu/Menu.js
@@ -1,14 +1,15 @@
 import { Link, useLocation } from 'react-router-dom';
+import './Menu.css';
 
 function Menu() {
 
   return(
-    <nav>
-      <ul>
-        <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/subscriptions'>Subscriptions</Link></li>
+    <nav className='header__navigation'>
+      <ul className='header__list'>
+        <li><Link className='header__link' to='/profile'>Profile</Link></li>
+        <li><Link className='header__link' to='/subscriptions'>Subscriptions</Link></li>
       </ul>
-      <button>Log out</button>
+      <button className='header__logout'>Log out</button>
     </nav>
   )
 }
