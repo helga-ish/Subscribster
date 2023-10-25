582bc9de9140b03a32f9fa46136f48535333e502
Helga Ish <belfegor.nomnom@gmail.com>
2023-10-25 18:43:51 +0800

    feat: add footer styles and logos

--- subscribster/src/components/Footer/Footer.js
+++ subscribster/src/components/Footer/Footer.js
@@ -1,18 +1,20 @@
 import './Footer.css';
 import { currentYear, creator } from '../../constants/const';
+import githubLogo from '../../images/githublogo.svg';
+import instaLogo from '../../images/instalogo.svg';
 
 export default function Footer() {
 
 
 
   return(
-    <footer>
-      <div>
-        <ul>
-          <li><a><img alt='instagram logo'/>Instagram</a></li>
-          <li><a href="https://github.com/helga-ish" target="_blank" rel='noreferrer'><img alt='github logo'/>GitHub</a></li>
+    <footer className='footer'>
+      <div className='footer__container'>
+        <ul className='footer__list'>
+          <li><a className='footer__link' href='https://instagram.com/helga_ish/' target="_blank" rel='noreferrer'><img className='footer__social-logo' src={instaLogo} alt='instagram'/> Instagram</a></li>
+          <li><a className='footer__link' href="https://github.com/helga-ish" target="_blank" rel='noreferrer'><img className='footer__social-logo' src={githubLogo} alt='github'/> GitHub</a></li>
         </ul>
-        <p>{`© ${currentYear}. ${creator}`}</p>
+        <p className='footer__year'>{`© ${currentYear}. ${creator}`}</p>
       </div>
     </footer>
   )
