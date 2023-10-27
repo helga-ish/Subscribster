d7378d818d11c0c8612bce501404985ee85b93d5
Helga Ish <belfegor.nomnom@gmail.com>
2023-10-27 19:19:25 +0800

    feat: add subscr gallery styles

--- subscribster/src/components/SubscriptionItem/SubscriptionItem.js
+++ subscribster/src/components/SubscriptionItem/SubscriptionItem.js
@@ -1,3 +1,18 @@
+import './SubscriptionItem.css';
+import netflixicon from '../../images/netflixicon.svg';
+
 export default function SubscriptionItem() {
-  
+
+  return(
+    <li className="subscriptionItem">
+      <div className="subscriptionItem__block">
+        <img className='subscriptionItem__icon' src={netflixicon} alt='icon'/>
+      </div>
+      <div className="subscriptionItem__info">
+        <h2 className="subscriptionItem__name">Netflix</h2>
+        <p className="subscriptionItem__status subscriptionItem__status_active subscriptionItem__status_notactive">Active</p>
+      </div>
+      <p className='subscriptionItem__expire'>Expires on 11/12/23</p>
+    </li>
+  )
 }
