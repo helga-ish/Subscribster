6fb1631bfbb361d8bdaede71a16b58b44a950694
Helga Ish <belfegor.nomnom@gmail.com>
2023-10-27 19:19:59 +0800

    feat: change active link

--- subscribster/src/components/Menu/Menu.js
+++ subscribster/src/components/Menu/Menu.js
@@ -7,7 +7,7 @@ function Menu() {
     <nav className='header__navigation'>
       <ul className='header__list'>
         <li><Link className='header__link' to='/profile'>Profile</Link></li>
-        <li><Link className='header__link' to='/subscriptions'>Subscriptions</Link></li>
+        <li><Link className='header__link header__link_active' to='/subscriptions'>My subscriptions</Link></li>
       </ul>
       <button className='header__logout'>Log out</button>
     </nav>
