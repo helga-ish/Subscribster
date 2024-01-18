cf5c1436a2c5bdccdb2f38278c84832428e3983b
Helga Ish <belfegor.nomnom@gmail.com>
2024-01-18 20:42:33 +0800

    feat: add working randomizer and display result

--- subscribster/src/components/Randomizer/Randomizer.js
+++ subscribster/src/components/Randomizer/Randomizer.js
@@ -5,6 +5,7 @@ export default function Randomizer() {
 
 const [newItem, setNewItem] = useState('');
 const [items, setItems] = useState([]);
+const [chosenWord, setChosenWord] = useState(null);
 
 useEffect(() => {
   const storedItems = sessionStorage.getItem('listItems');
@@ -29,15 +30,31 @@ const resetList = () => {
 
   sessionStorage.removeItem('listItems');
 }
-let result;
+
 const getRandomItem = () => {
   const storedItems = sessionStorage.getItem('listItems');
+
   if (storedItems) {
-    setItems(JSON.parse(storedItems));
+    const parsedItems = JSON.parse(storedItems);
+    setItems(parsedItems);
+    console.log(parsedItems);
+
+    const filteredItems = parsedItems.filter(item => item !== "" && !/[&\|\/\\]/.test(item));
+
+    if (filteredItems.length > 0) {
+      const result = filteredItems[Math.floor(Math.random() * filteredItems.length)];
+      console.log(result);
+      setChosenWord(result)
+      return result;
+    } else {
+      console.log("No valid items in the array.");
+      return null;
+    }
+  } else {
+    console.log("No stored items found.");
+    setChosenWord("Add items to the list first.")
+    return null;
   }
-  result = storedItems[Math.floor(Math.random()*storedItems.length)];
-  console.log(result);
-  return result;
 }
 
   return(
@@ -53,7 +70,7 @@ const getRandomItem = () => {
         <button onClick={getRandomItem}>Choose randomly from the list</button>
         <div>
           <p>Your result:</p>
-          <p>{result}</p>
+          <p>{chosenWord}</p>
         </div>
       </div>
       <button onClick={resetList}>Reset list</button>
