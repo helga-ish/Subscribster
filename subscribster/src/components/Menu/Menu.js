9881ff4595014f8ac241018ad3f95175917609c1
Helga Ish <belfegor.nomnom@gmail.com>
2024-02-01 20:54:54 +0800

    feat: started changing links highlight

--- subscribster/src/components/Menu/Menu.js
+++ subscribster/src/components/Menu/Menu.js
@@ -1,14 +1,28 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Menu.css';
+import React from 'react';
 
 function Menu() {
+
+  const [isActive, setIsActive] = React.useState(false);
+    function handleActiveState() {
+        setIsActive(true);
+    }
+    function handleInactiveState() {
+        setIsActive(false);
+    }
+  const profileLinkClassname = `header__link ${isActive ? 'header__link_active' : ''}`;
+  const subscrLinkClassname = `header__link ${isActive ? 'header__link_active' : ''}`;
+  const randomLinkClassname = `header__link ${isActive ? 'header__link_active' : ''}`;
+
+
 
   return(
     <nav className='header__navigation'>
       <ul className='header__list'>
-        <li><Link className='header__link' to='/profile'>Profile</Link></li>
-        <li><Link className='header__link header__link_active' to='/subscriptions'>My subscriptions</Link></li>
-        <li><Link className='header__link' to='/randomizer'>My RANDOMIZER</Link></li>
+        <li><Link className={profileLinkClassname} onClick={!isActive ? handleActiveState : handleInactiveState} to='/profile'>Profile</Link></li>
+        <li><Link className={subscrLinkClassname} onClick={!isActive ? handleActiveState : handleInactiveState} to='/subscriptions'>My subscriptions</Link></li>
+        <li><Link className={randomLinkClassname} onClick={!isActive ? handleActiveState : handleInactiveState} to='/randomizer'>My Randomizer</Link></li>
       </ul>
       <button className='header__logout'>Log out</button>
     </nav>
